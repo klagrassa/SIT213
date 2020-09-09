ff2e1c86ed6cf65e532cd8fbb3f541c33f272ad5
Elia-lab <71017465+Elia-lab@users.noreply.github.com>
2020-09-09 16:28:10 +0200

    Add files via upload

---
+++ src/emetteur/Emetteur.java
@@ -0,0 +1,56 @@
+package emetteur;
+
+import destinations.DestinationInterface;
+import information.Information;
+import information.InformationNonConforme;
+import transmetteurs.Transmetteur;
+
+import java.util.Iterator;
+
+public class Emetteur extends Transmetteur<Boolean,Float> {
+    private Float ampMax;
+    private Float ampMin;
+    private String mode;
+    private Information<Float> informationGenere= null;
+    private int pasEchantillonage;
+
+
+
+    public Emetteur(Float ampMax, Float ampMin, int pasEchantillonage) {
+        this.ampMax = ampMax;
+        this.ampMin = ampMin;
+        this.mode ="NRZ";
+        this.pasEchantillonage=pasEchantillonage;
+    }
+
+    @Override
+    public void recevoir(Information<Boolean> information) throws InformationNonConforme {
+        informationRecue = information;
+        informationGenere = new Information<Float>();
+        Iterator it = informationRecue.iterator();
+        while (it.hasNext()) {
+            if (it.next() == Boolean.TRUE) {
+                ajouterSymbole(ampMax);
+            } else ajouterSymbole(ampMin);
+
+        }
+        this.emettre();
+
+
+    }
+    protected void ajouterSymbole(Float amp){
+        for (int i = 0; i<pasEchantillonage; i++){
+            informationGenere.add(amp);}
+    }
+
+    @Override
+    public void emettre() throws InformationNonConforme {
+        this.informationEmise = this.informationGenere;
+        // émission vers les composants connectés
+        for (DestinationInterface<Float> destinationConnectee : destinationsConnectees) {
+            destinationConnectee.recevoir(informationEmise);
+        }
+    }
+
+
+}
