4456b4e28b70cf2ae33cd22ab66f117fb1322f78
Elia <elia.dumont@imt-atlantique.net>
2020-09-17 12:18:50 +0200

    NRZT avec prise en compte Echantillon non multiple de 3

--- src/emetteurs/EmetteurNRZT.java
+++ src/emetteurs/EmetteurNRZT.java
@@ -6,7 +6,7 @@ import information.InformationNonConforme;
 /**
  * Implémentation de l'émetteur NRZT. Les contraintes et la forme de ce signal
  * sont disponibles dans les consignes de l'étape 2.
- * 
+ *
  * @author Groupe 3
  *
  * @param <R> : Type d'info en entrée (recepteur)
@@ -29,105 +29,103 @@ public class EmetteurNRZT<R, E> extends Emetteur<R, E> {
 	/**
 	 * Reçoit l'information de l'interface désignée en reception, puis convertit le
 	 * signal logique en signal de type NRZT
-	 * 
+	 *
 	 * @param information - information à recevoir
 	 */
 	public void recevoir(Information<Boolean> information) throws InformationNonConforme {
 		this.informationRecue = information;
 		this.informationGenere = new Information<Float>();
 
+		Float modulo = this.pasEchantillonage % 3.0F;
+
+		float nbEchantillonsPente = 0;
+		float nbEchantillonsPalier = 0;
+
+		if (modulo==1) {
+			nbEchantillonsPalier = (this.pasEchantillonage + 2) / 3.0F;
+			nbEchantillonsPente = (this.pasEchantillonage - 1) / 3.0F;
+		}
+		else if (modulo == 2) {
+			nbEchantillonsPalier = (this.pasEchantillonage - 2) / 3.0F;
+			nbEchantillonsPente = (this.pasEchantillonage + 1) / 3.0F;
+		}
+		else {
+			nbEchantillonsPalier = this.pasEchantillonage / 3.0F;
+			nbEchantillonsPente = this.pasEchantillonage / 3.0F;
+		}
+
 		Boolean front = null;
-		this.penteTransition = ((this.ampMax - ampMin) / 2) / ((float) this.pasEchantillonage / 3.0F);
+		this.penteTransition = ((this.ampMax - ampMin) / 2) / nbEchantillonsPente;
 
 		for (Boolean bitLogique : this.informationRecue) {
 			if (front == null) {
 				front = bitLogique;
 				if (bitLogique == Boolean.TRUE) {
-					int balise = 0;
-					for (int i = 0; i < (2 * this.pasEchantillonage / 3f)
-							* (ampMax / (ampMax + Math.abs(ampMin))); ++i) {
+					for (int i = 0; i < nbEchantillonsPente; ++i) {
 						this.informationGenere.add(0f - this.transition);
-						this.transition += -penteTransition;
-						balise++;
+						this.transition -= (this.ampMax/ nbEchantillonsPente);
 					}
-					while (balise < this.pasEchantillonage / 3) {
-						this.informationGenere.add(this.ampMax);
-						balise++;
-					}
-					this.ajouterSymbole(this.ampMax);
-				} else {
-					// this.ajouterTransition(((this.ampMax + ampMin)/2), this.penteTransition);
-					int balise = 0;
-					for (int i = 0; i < (2 * this.pasEchantillonage / 3f)
-							* (Math.abs(ampMin) / (ampMax + Math.abs(ampMin))); ++i) {
+					this.ajouterSymbole(this.ampMax, nbEchantillonsPalier);
+				}
+				else {
+					for (int i = 0; i < nbEchantillonsPente; ++i) {
 						this.informationGenere.add(0f - this.transition);
-						this.transition += penteTransition;
-						balise++;
+						this.transition += (Math.abs(this.ampMin)/ nbEchantillonsPente);
 					}
-					while (balise < this.pasEchantillonage / 3) {
-						this.informationGenere.add(this.ampMin);
-						balise++;
-					}
-
-					this.ajouterSymbole(ampMin);
+					this.ajouterSymbole(this.ampMin,nbEchantillonsPalier );
 
 				}
 			} else if (front != bitLogique) {
 				front = bitLogique;
 				if (bitLogique == Boolean.TRUE) {
 					this.transition = 0f;
-					this.ajouterTransition(this.ampMin, -this.penteTransition);
-					this.ajouterTransition(this.ampMin, -this.penteTransition);
-					this.ajouterSymbole(ampMax);
+					this.ajouterTransition(this.ampMin, -this.penteTransition, 2*nbEchantillonsPente);
+					this.ajouterSymbole(ampMax, nbEchantillonsPalier);
 
 				} else {
 					this.transition = 0f;
-					this.ajouterTransition(this.ampMax, this.penteTransition);
-					this.ajouterTransition(this.ampMax, this.penteTransition);
-					this.ajouterSymbole(ampMin);
+					this.ajouterTransition(this.ampMax, this.penteTransition, 2*nbEchantillonsPente);
+					this.ajouterSymbole(ampMin, nbEchantillonsPalier);
 
 				}
 			} else if (bitLogique == Boolean.TRUE) {
-				for (int i = 0; i < 3; i++)
-					this.ajouterSymbole(this.ampMax);
+				this.ajouterSymbole(this.ampMax, (float) this.pasEchantillonage);
 			} else {
-				for (int i = 0; i < 3; i++)
-					this.ajouterSymbole(this.ampMin);
+				this.ajouterSymbole(this.ampMin, (float) this.pasEchantillonage);
 			}
 		}
-		// Bastien idea
-		int padding = informationRecue.nbElements() * this.pasEchantillonage;
-		while (informationGenere.nbElements() < padding) {
-			this.informationGenere.add(this.informationGenere.iemeElement(informationGenere.nbElements() - 1));
-		}
-
+		this.ajouterSymbole(this.informationGenere.iemeElement(informationGenere.nbElements() - 1), nbEchantillonsPente);
 		this.emettre();
 	}
 
 	/**
 	 * Ajoute le "plateau" de valeur amp au signal
-	 * 
+	 *
 	 * @param amp - amplitude du "plateau"
 	 */
-	@Override
-	protected void ajouterSymbole(Float amp) {
-		for (int i = 0; i < pasEchantillonage / 3; i++) {
+	protected void ajouterSymbole(Float amp, Float nbEchantillonsPalier) {
+		for (int i = 0; i < nbEchantillonsPalier; i++) {
 			informationGenere.add(amp);
 		}
 	}
 
 	/**
 	 * Ajoute le motif de transition (haute ou basse)
-	 * 
+	 *
 	 * @param amp             - amplitude désirée (vers quelle valeur la transition
 	 *                        doit s'effectuer)
 	 * @param penteTransition - pente de la transition
 	 */
-	private void ajouterTransition(Float amp, Float penteTransition) {
-		for (int i = 0; i < this.pasEchantillonage / 3; ++i) {
+	private void ajouterTransition(Float amp, Float penteTransition, Float nbEchantillonsPente) {
+		for (int i = 0; i < nbEchantillonsPente; ++i) {
 			this.informationGenere.add(amp - this.transition);
 			this.transition += penteTransition;
 		}
+	}
+
+	@Override
+	protected void ajouterSymbole(Float amp) {
+		// TODO Auto-generated method stub
 
 	}
 }
