034e1d91986045c427ba51bc031ba11b93cf0726
Siohbann <59995911+Siohbann@users.noreply.github.com>
2020-09-30 14:37:40 +0200

    Update Codeur.java

--- src/codeur/Codeur.java
+++ src/codeur/Codeur.java
@@ -1,4 +1,40 @@
-package codeur;
+package codeurs;
 
-public class Codeur {
+import java.util.LinkedList;
+
+import app.ArgumentsException;
+import destinations.DestinationInterface;
+import information.Information;
+import information.InformationNonConforme;
+import transmetteurs.Transmetteur;
+
+
+public class Codeur<R,E> extends Transmetteur<Boolean, Boolean> {
+	
+    public Codeur() {
+    	super();
+    }
+	
+	public void recevoir(Information<Boolean> informationRecue) throws InformationNonConforme {
+        for (Boolean aBoolean : informationRecue) {
+            if (aBoolean == Boolean.TRUE) {
+            	informationEmise.add(Boolean.TRUE);
+            	informationEmise.add(Boolean.FALSE);
+            	informationEmise.add(Boolean.TRUE);
+            } 
+            else {
+            	informationEmise.add(Boolean.FALSE);
+            	informationEmise.add(Boolean.TRUE);
+            	informationEmise.add(Boolean.FALSE);
+            }
+        }
+        this.emettre();
+	}
+
+	public void emettre() throws InformationNonConforme {
+		// Ã©mission vers les composants connectÃ©s
+		for (DestinationInterface<E> destinationConnectee : destinationsConnectees) {
+			destinationConnectee.recevoir(informationEmise);
+		}
+	}
 }
